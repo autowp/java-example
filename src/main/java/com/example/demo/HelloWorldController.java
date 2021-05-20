42f4ba2feaa62e57cebe925945302365d1645cd4
Dmitry Pereslegin <pereslegin@vsk.ru>
2021-05-20 15:48:56 +0300

    ascii art

--- src/main/java/com/example/demo/HelloWorldController.java
+++ src/main/java/com/example/demo/HelloWorldController.java
@@ -1,12 +1,30 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UncheckedIOException;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
 @RestController
 public class HelloWorldController {
-    @GetMapping("/helloworld")
+
+    @Value("classpath:demo.txt")
+    private Resource resource;
+
+    @GetMapping(value = "/", produces = "text/plain; charset=utf-8")
     public String helloWorld() {
-        return "helloworld";
+        try (Reader reader = new InputStreamReader(resource.getInputStream(), UTF_8)) {
+            return FileCopyUtils.copyToString(reader);
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
     }
 }
